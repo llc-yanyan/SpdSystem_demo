782b2a710b182aab65a770c71be1d37734d06c7c
yanai <yanaitti@gmail.com>
2018-01-27 22:59:38 +0900

    updates

--- src/main/java/com/example/demo/Dealer.java
+++ src/main/java/com/example/demo/Dealer.java
@@ -7,8 +7,48 @@ import org.springframework.ui.Model;
 @Controller
 public class Dealer {
 
-    @RequestMapping("dealer/login")
+    @RequestMapping("dealer/")
     public String login(Model model) {
-    		return "a-1_login";
+    		return "a-1";
+    }
+
+    @RequestMapping("dealer/passwordreissue/")
+    public String passwordreissue(Model model) {
+    		return "a-2";
+    }
+
+    @RequestMapping("dealer/authcode/")
+    public String authcode(Model model) {
+    		return "a-3";
+    }
+
+    @RequestMapping("dealer/resetpassword/")
+    public String resetpassword(Model model) {
+    		return "a-4";
+    }
+
+    @RequestMapping("dealer/registpassword/")
+    public String registpassword(Model model) {
+    		return "a-5";
+    }
+
+    @RequestMapping("dealer/logout")
+    public String logout(Model model) {
+    		return "a-6";
+    }
+
+    @RequestMapping("dealer/orderreceive/")
+    public String orderreceive(Model model) {
+    		return "b-1";
+    }
+
+    @RequestMapping("dealer/contactlist/")
+    public String contactlist(Model model) {
+    		return "c-1";
+    }
+
+    @RequestMapping("dealer/help/")
+    public String help(Model model) {
+    		return "d-1";
     }
 }
